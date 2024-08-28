70759e6c636ce603a4c54316a4f0df1ea0b75285
Samet Can Incesu <saincesu@k1m07s01.42kocaeli.com.tr>
2024-08-28 12:57:10 +0300

    C_02

---
+++ C_02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 13:50:55 by saincesu          #+#    #+#             */
+/*   Updated: 2024/08/25 13:50:56 by saincesu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ C_02/ex09/ft_strcapitalize_main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize_main.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 14:01:01 by saincesu          #+#    #+#             */
+/*   Updated: 2024/08/25 14:03:00 by saincesu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		i1;
+
+	i = 0;
+	i1 = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i1 == 1)
+				str[i] -= 32;
+			i1 = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			i1 = 0;
+		else
+			i1 = 1;
+		i++;
+	}
+	return (str);
+}
